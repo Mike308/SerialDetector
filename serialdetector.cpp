64acc2fde751a616712a4d96c8ad5dbb4992ed97
Majkel44 <m.werner9205@gmail.com>
2017-03-12 11:22:47 +0100

    - added definition of SerialDetector class - implemented constructor of SerialDetector class - in constructor:   *added thread to handling scanning SerialPorts   *added  serialScanner object   *moving serialScanner object to thread scannThread   *connecting start signal to slot scannSerialPorts   *connecting scannSignal to handleResults slot - implemented SerialDetector destructor where thread is quitting and setting to wait - added definition of handleResults slot where is emitting onDetect signal

---
+++ serialdetector.cpp
@@ -0,0 +1,27 @@
+#include "serialdetector.h"
+
+SerialDetector::SerialDetector()
+{
+    scannThread = new QThread();
+    serialScanner = new SerialScanner();
+    serialScanner->moveToThread(scannThread);
+    connect(scannThread,SIGNAL(started()),serialScanner,SLOT(scannSerialPorts()));
+    connect(serialScanner,SIGNAL(scannSignal(int,QSet<QString>)),this,SLOT(handleResults(int,QSet<QString>)));
+    scannThread->start();
+
+}
+
+SerialDetector::~SerialDetector(){
+
+    scannThread->quit();
+    scannThread->wait();
+}
+
+void SerialDetector::handleResults(const int &status, const QSet<QString> &coms){
+
+
+    emit onDetect(status,coms);
+
+}
+
+
