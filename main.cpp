042f72e898475e7ab8837e842f2b0d0eedd2496d
Majkel44 <m.werner9205@gmail.com>
2017-03-12 11:24:06 +0100

    - added registering QSet to avoiding error: Cannot queue arguments of type 'QSet<QString>'

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSet>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    qRegisterMetaType<QSet<QString>>();
 
     return a.exec();
 }
